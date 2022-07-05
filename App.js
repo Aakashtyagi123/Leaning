8d602ea34977d508e7759aa456b5fe9b4e9a416a
Aakashtyagi123 <aakashtyagi1723@gmail.com>
2022-07-05 21:53:57 +0530

    changes

---
+++ App.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import {View,Text} from "react-native"
+function App() {
+  return (
+    <View><Text>Happy</Text></View>
+  )
+}
+
+export default App
---
+++ android/as.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function as() {
+  return (
+    <div>as</div>
+  )
+}
+
+export default as
